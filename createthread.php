3595de3e6266d475ae1e0d78636089eb18b4d6c4
fooleap <fooleap@gmail.com>
2017-04-01 14:31:47 +0800

    add create thread

---
+++ createthread.php
@@ -0,0 +1,14 @@
+<?php
+    namespace Emojione;
+    require_once('init.php');
+    $curl_url = 'https://disqus.com/api/3.0/threads/create.json';
+    $post_data = array(
+        'api_key' => $public_key,
+        'forum' => $forum,
+        'message' => $_POST['message'],
+        'slug' => $_POST['slug'],
+        'title' => $_POST['title'],
+        'url' => $origin.$_POST['url']
+    );
+    $data = curl_post($curl_url, $post_data);
+    print_r(json_encode($data)); 
